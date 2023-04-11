77d05b002b3298379b900e4fa046b578a00b171e
Kian-Wee <kianweetan2002@gmail.com>
2023-04-11 14:32:33 +0800

    cleaned up code, added euler publisher

--- SCRIPT1_2_StreamDataROS2.m
+++ SCRIPT1_2_StreamDataROS2.m
@@ -6,26 +6,10 @@ clear all
 close all
 clc
 
-import optitrack_class
-
-% %% Variables
-bodyname='bigchungus'; % rb name in optitrack
-% hydra1_pose_topic_name = "/hydra1/optitrack_pose"; 
-% 
-optitrack_node = ros2node("/matlab_optitrack_publisher");
-
+import optitrack_class.*
 
 %% Create OptiTrack object, ROS node and publisher
-% hydra1_pose_pub = ros2publisher(optitrack_node, hydra1_pose_topic_name, "geometry_msgs/PoseStamped");
-% hydra1_pose_msg = ros2message(hydra1_pose_pub);
-% hydra1_pose_msg.pose.position.x = 1;
-% hydra1_pose_msg.pose.position.y = 0;
-% hydra1_pose_msg.pose.position.z = 0;
-% hydra1_pose_msg.pose.orientation.w = 1;
-% hydra1_pose_msg.pose.orientation.x = 0;
-% hydra1_pose_msg.pose.orientation.y = 0;
-% hydra1_pose_msg.pose.orientation.z = 0;
-
+optitrack_node = ros2node("/matlab_optitrack_publisher");
 obj = OptiTrack;
 Initialize(obj,'192.168.1.5','multicast'); %Ensure broadcast frame id is on, loop interface is set to this ip and transmission type is set to multicast
 
@@ -45,13 +29,12 @@ for i = 1:numel(rb)
     my_field = strcat(convertCharsToStrings(rb(i).Name));
     variable.(my_field) = optitrack_class;
     variable.(my_field).init(convertCharsToStrings(rb(i).Name),optitrack_node);
-
 end
 
 disp('Outputing poses for: ');
 disp(variable)
 
-%%
+%% Loop
 
 while true    
     % Get current rigid body information, this has to be recalled every time for new frame index
@@ -61,33 +44,24 @@ while true
     % fprintf('\nFrame Index: %d\n',rb(1).FrameIndex);
     % Update each rigid body
     for i = 1:numel(rb)
-%         disp(i)
-%         disp(rb(i))
-        % Check for correct body
         % This crashes if there is a glitching body, remove any unused body
         % from the asset tab, if not uncomment this line
-%             fprintf(rb(i).Name);
             if isempty(rb(i).Position)== 0 % Check that position array is properly formed(not missing)
                 rb_position = rb(i).Position/1000;
                 rb_quaternion = rb(i).Quaternion; % w, x, y, z
                    
 %                 fprintf('%s\t   Position [%f,%f,%f]\n', rb(i).Name, rb_position);
-    %             fprintf('\t Quaternion [%f,%f,%f,%f]\n',rb_quaternion );
+%                 fprintf('\t Quaternion [%f,%f,%f,%f]\n',rb_quaternion );
 %                 hydra1_pose_msg.pose.position.x = rb_position(1);
 %                 hydra1_pose_msg.pose.position.y = rb_position(2);            
 %                 hydra1_pose_msg.pose.position.z = rb_position(3);
-%                    
 %                 hydra1_pose_msg.pose.orientation.w = rb_quaternion(1,1);
 %                 hydra1_pose_msg.pose.orientation.x = rb_quaternion(1,2);
 %                 hydra1_pose_msg.pose.orientation.y = rb_quaternion(1,3);
 %                 hydra1_pose_msg.pose.orientation.z = rb_quaternion(1,4);
+
                 my_field = strcat(convertCharsToStrings(rb(i).Name));
                 variable.(my_field).sendpose(rb_position(1),rb_position(2),rb_position(3),rb_quaternion(1,1),rb_quaternion(1,2),rb_quaternion(1,3),rb_quaternion(1,4));
-%                 send(variable.(my_field).body_pose_pub,variable.(my_field).body_pose_msg);
-                %                 t = cast(posixtime(datetime), "int32");
-%                 hydra1_pose_msg.header.stamp.sec = t;
-%                 hydra1_pose_msg.header.frame_id ='/optitrack1';
-%                 send(hydra1_pose_pub,hydra1_pose_msg); % publish msg
             end
 %         else
 %             arr(end+1) = rb(i).Name;
--- optitrack_class.m
+++ optitrack_class.m
@@ -12,19 +12,29 @@ classdef optitrack_class < handle
          obj.body_output_topic = strcat(obj_name,"/optitrack_pose");
          obj.body_pose_pub = ros2publisher(optitrack_node, obj.body_output_topic, "geometry_msgs/PoseStamped");
          obj.body_pose_msg = ros2message("geometry_msgs/PoseStamped");
-
       end
+
       function sendpose(obj,x,y,z,ww,wx,wy,wz)
         obj.body_pose_msg.pose.position.x = x;
         obj.body_pose_msg.pose.position.y = y;
         obj.body_pose_msg.pose.position.z = z;
-        obj.body_pose_msg.pose.orientation.w = ww;
-        obj.body_pose_msg.pose.orientation.x = wx;
-        obj.body_pose_msg.pose.orientation.y = wy;
-        obj.body_pose_msg.pose.orientation.z = wz;
+%         obj.body_pose_msg.pose.orientation.w = ww;
+%         obj.body_pose_msg.pose.orientation.x = wx;
+%         obj.body_pose_msg.pose.orientation.y = wy;
+%         obj.body_pose_msg.pose.orientation.z = wz;
+
+        % Janky hack for euler on the same topic;
+        eu=quat2eul(ww,wx,wy,wz);
+        obj.body_pose_msg.pose.orientation.w = 0;
+        obj.body_pose_msg.pose.orientation.x = eu(2);
+        obj.body_pose_msg.pose.orientation.y = eu(1);
+        obj.body_pose_msg.pose.orientation.z = eu(0);
+        disp(quat2eul(ww,wx,wy,wz));
+
         obj.body_pose_msg.header.stamp.sec = cast(posixtime(datetime), "int32");
         obj.body_pose_msg.header.frame_id ='/odom';
         send(obj.body_pose_pub,obj.body_pose_msg); % publish msg
+        
       end
    end
 end
