63f3896cae9c0e07a5d503de55906a1f7e7d7b79
KW <45945080+Kian-Wee@users.noreply.github.com>
2023-07-04 10:44:41 +0800

    bugfix px4 uxrce message

--- optitrack_class.m
+++ optitrack_class.m
@@ -13,7 +13,8 @@ classdef optitrack_class < handle
          obj.body_output_topic = strcat(obj_name,"/optitrack_pose");
          obj.mode=mode;
          if mode=="px4" %Assume publishing using uXRCE-DDS PX4ROS2 bridge
-            obj.body_pose_pub = ros2publisher(optitrack_node, "/fmu/in/vehicle_mocap_odometry", "px4_msgs/VehicleOdometry","History","keeplast","depth",0,"Reliability","besteffort","Durability","transientlocal");
+            % obj.body_pose_pub = ros2publisher(optitrack_node, "/fmu/in/vehicle_visual_odometry", "px4_msgs/VehicleOdometry","History","keeplast","depth",0,"Reliability","besteffort","Durability","transientlocal");
+            obj.body_pose_pub = ros2publisher(optitrack_node, "/fmu/in/vehicle_visual_odometry", "px4_msgs/VehicleOdometry")
             obj.body_pose_msg = ros2message("px4_msgs/VehicleOdometry");
          else 
             obj.body_pose_pub = ros2publisher(optitrack_node, obj.body_output_topic, "geometry_msgs/PoseStamped");
@@ -24,16 +25,18 @@ classdef optitrack_class < handle
       function sendpose(obj,x,y,z,ww,wx,wy,wz)
 
       if obj.mode=="px4"
-         obj.body_pose_msg.timestamp = uint64(rostime("now").Sec*10^9+rostime("now").Nsec);
-         obj.body_pose_msg.timestamp_sample = uint64(rostime("now").Sec*10^9+rostime("now").Nsec);
-         obj.body_pose_msg.pose_frame_ned= 1; % NED earth-fixed frame;
+         obj.body_pose_msg.timestamp = uint64((rostime("now").Sec*10^9+rostime("now").Nsec)/1000);
+         % obj.body_pose_msg.timestamp_sample = uint64(rostime("now").Sec*10^9+rostime("now").Nsec); %using this causes it not to fuse for some reason
+         obj.body_pose_msg.pose_frame = uint8(2); % 1 for NED earth-fixed frame; 2 for FRD arbitrary frame
          obj.body_pose_msg.position(1)=x;
-         obj.body_pose_msg.position(2)=y;
-         obj.body_pose_msg.position(3)=z;
-         obj.body_pose_msg.q(1)=ww;
-         obj.body_pose_msg.q(2)=wx;
-         obj.body_pose_msg.q(3)=wy;
-         obj.body_pose_msg.q(4)=wz;
+         obj.body_pose_msg.position(2)=-y;
+         obj.body_pose_msg.position(3)=-z;
+         xx = [[1 0 0];
+               [0 cos(pi) -sin(pi)];
+               [0 sin(pi) cos(pi)];];
+         q1 = rotm2quat(xx);
+         qq = quatmultiply(q1,[ww,wx,wy,wz]);
+         obj.body_pose_msg.q =single(qq);
       else
          obj.body_pose_msg.header.stamp.sec = cast(posixtime(datetime), "int32");
          obj.body_pose_msg.header.frame_id ='/odom';
